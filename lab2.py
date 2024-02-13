61e3695b39dc5d3d189429d078c8f341285a1fdf
nhanda0 <159860532+nhanda0@users.noreply.github.com>
2024-02-12 22:39:08 -0500

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -1,4 +1,7 @@
+import sys
 
-def helloWorld():
-      print ('Hello World' )
-helloWorld()
+def print_args():
+    script_name = sys.argv[0]
+    arguments = sys.argv[1:]
+    print("Script:", script_name)
+    print("Arguments:",arguments)
